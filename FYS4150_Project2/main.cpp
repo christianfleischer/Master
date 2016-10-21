f89ae26be313436453e62a8fb6c42e7d27c601df
Alex Fleischer <fleisch91@gmail.com>
2016-10-21 18:13:32 +0200

    failed making progress bar, probably no change since last commit ...

--- FYS4150_Project2/main.cpp
+++ FYS4150_Project2/main.cpp
@@ -14,7 +14,7 @@ int main() {
     double posMin               = -10;
     double posMax               = 10;
     double omega_r              = 0.5;                                         // =m*w/hbar Just a constant to keep the results correct, while we figure out the omega conundrum.
-    int nMax 					= 20;
+    int nMax 					= 30;
     int nPrimeMax               = 3;
 
     int numberOfEigstates       = 100;
