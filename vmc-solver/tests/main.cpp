b1615f4e08386e86518170987862c9f9e506a2e5
Alexander Fleischer <fleisch91@gmail.com>
2017-02-07 18:14:40 +0100

    added wrapper for vmc-solver

--- vmc-solver/tests/main.cpp
+++ vmc-solver/tests/main.cpp
@@ -31,105 +31,104 @@ using namespace UnitTest;
 
 
 
-SUITE(QD) {
-TEST(Sanity) {CHECK_EQUAL(1,1);}
+SUITE(VMCsolver) {
+    TEST(Sanity) {CHECK_EQUAL(1,1);}
+
+    Wrapper* wrapper = new Wrapper();
 
     TEST(Initialization) {
-        int my_rank = 0;
-        int numprocs = 4;
-        double totalE, totalKE, totalPE, totalVariance, totalAcceptanceRate, finalMeanDistance;
-        double timeStart, timeEnd, totalTime;
-
-        vec L(3);
-        L.fill(0.);
-
-        int numberOfDimensions  = 3;
-        int numberOfParticles   = 2;
-        int numberOfSteps       = (int) 1e6;
-        double omega            = 1.;
-        double alpha            = 0.98456;
-        double beta             = 0.40691;
-        double gamma            = 2.82843;
-        double a                = 0.0043;
-        double stepLength       = 0.5;
-        double equilibration    = 0.1;
-        double dt               = 0.01;
-        double aElectrons       = 1.;
-        double C                = 1.;
-        bool analyticalKinetic  = false;
-        bool importanceSampling = true;
-        bool repulsion          = true;
-        bool quantumDots        = true;
-        bool twobodyQD          = false;
-        bool Jastrow            = true;
-        bool optimizeParameters = false;
-        bool saveEnergies       = false;
-        bool savePositions      = false;
-        bool showProgress       = true;
-        bool printToTerminal    = true;
-        bool useCoeff 		    = false;
-
-        int numMyCycles = numberOfSteps/numprocs;
-        System* system = new System();
 
         // Initialize MPI parallelization
-        MPI_Init(0,0);
-        MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-        MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-        timeStart = MPI_Wtime();
+        //MPI_Init(&nargs, &args);
+        //MPI_Comm_size(MPI_COMM_WORLD, &wrapper->m_numprocs);
+        //MPI_Comm_rank(MPI_COMM_WORLD, &wrapper->m_my_rank);
+        wrapper->m_numprocs = 1;
+        wrapper->m_my_rank = 0;
+        //wrapper->m_timeStart = MPI_Wtime();
+        wrapper->m_numberOfDimensions  = 1;
 
-        system->setInitialState             (new RandomUniform(system, numberOfDimensions, numberOfParticles, my_rank));
+        vec L(wrapper->m_numberOfDimensions);
+        L.fill(0.);
+        L(0) = 0.;
+        wrapper->setL(L);
 
-        if (repulsion && !quantumDots) {
-        system->setHamiltonian              (new HarmonicOscillatorRepulsive(system, omega, a, gamma, analyticalKinetic));
-        system->setWaveFunction             (new RepulsiveGaussian(system, alpha, beta, a));
+        wrapper->m_numberOfParticles   = 2;
+        wrapper->m_numberOfSteps       = (int) 1e6;
+        wrapper->m_omega            = 1.;
+        wrapper->m_alpha            = 0.98456;
+        wrapper->m_beta             = 0.40691;
+        wrapper->m_gamma            = 2.82843;
+        wrapper->m_a                = 0.0043;
+        wrapper->m_stepLength       = 0.5;
+        wrapper->m_equilibration    = 0.1;
+        wrapper->m_dt               = 0.01;
+        wrapper->m_aElectrons       = 1.;
+        wrapper->m_C                = 1.;
+        wrapper->m_analyticalKinetic  = false;
+        wrapper->m_importanceSampling = true;
+        wrapper->m_repulsion          = true;
+        wrapper->m_quantumDots        = true;
+        wrapper->m_twobodyQD          = false;
+        wrapper->m_Jastrow            = true;
+        wrapper->m_optimizeParameters = false;
+        wrapper->m_saveEnergies       = false;
+        wrapper->m_savePositions      = false;
+        wrapper->m_showProgress       = true;
+        wrapper->m_printToTerminal    = true;
+        wrapper->m_useCoeff 		  = false;
+
+        System* system = new System();
+
+        system->setInitialState             (new RandomUniform(system, wrapper->m_numberOfDimensions, wrapper->m_numberOfParticles, wrapper->m_my_rank));
+
+        if (wrapper->m_repulsion && !wrapper->m_quantumDots) {
+            system->setHamiltonian              (new HarmonicOscillatorRepulsive(system, wrapper->m_omega, wrapper->m_a, wrapper->m_gamma, wrapper->m_analyticalKinetic));
+            system->setWaveFunction             (new RepulsiveGaussian(system, wrapper->m_alpha, wrapper->m_beta, wrapper->m_a));
         }
-        if (!repulsion && !quantumDots) {
-        system->setHamiltonian              (new HarmonicOscillator(system, omega, analyticalKinetic));
-        system->setWaveFunction             (new SimpleGaussian(system, alpha));
+        if (!wrapper->m_repulsion && !wrapper->m_quantumDots) {
+            system->setHamiltonian              (new HarmonicOscillator(system, wrapper->m_omega, wrapper->m_analyticalKinetic));
+            system->setWaveFunction             (new SimpleGaussian(system, wrapper->m_alpha));
         }
-        if (quantumDots) {
-            if (twobodyQD) {
-                system->setHamiltonian      (new HarmonicOscillatorElectrons(system, omega, analyticalKinetic, repulsion));
-                system->setWaveFunction     (new TwoElectrons(system, alpha, beta, omega, aElectrons, C, Jastrow));
+        if (wrapper->m_quantumDots) {
+            if (wrapper->m_twobodyQD) {
+                system->setHamiltonian      (new HarmonicOscillatorElectrons(system, wrapper->m_omega, wrapper->m_analyticalKinetic, wrapper->m_repulsion));
+                system->setWaveFunction     (new TwoElectrons(system, wrapper->m_alpha, wrapper->m_beta, wrapper->m_omega, wrapper->m_aElectrons, wrapper->m_C, wrapper->m_Jastrow));
             }
             else {
-                system->setHamiltonian      (new HarmonicOscillatorElectrons(system, omega, analyticalKinetic, repulsion));
-                if (useCoeff) {
-                    system->setWaveFunction (new ManyElectronsCoefficients(system, alpha, beta, omega, C, Jastrow));
+                system->setHamiltonian      (new HarmonicOscillatorElectrons(system, wrapper->m_omega, wrapper->m_analyticalKinetic, wrapper->m_repulsion));
+                if (wrapper->m_useCoeff) {
+                    system->setWaveFunction (new ManyElectronsCoefficients(system, wrapper->m_alpha, wrapper->m_beta, wrapper->m_omega, wrapper->m_C, wrapper->m_Jastrow));
                 }
                 else {
-                    system->setWaveFunction (new ManyElectrons(system, alpha, beta, omega, C, Jastrow));
+                    system->setWaveFunction (new ManyElectrons(system, wrapper->m_alpha, wrapper->m_beta, wrapper->m_omega, wrapper->m_C, wrapper->m_Jastrow));
                 }
             }
         }
-        system->setEquilibrationFraction    (equilibration);
-        system->setStepLength               (stepLength);
-        system->setTimeStep                 (dt);
-        system->setMyRank                   (my_rank);
-        system->setNumProcs                 (numprocs);
+        system->setEquilibrationFraction    (wrapper->m_equilibration);
+        system->setStepLength               (wrapper->m_stepLength);
+        system->setTimeStep                 (wrapper->m_dt);
+        system->setMyRank                   (wrapper->m_my_rank);
 
-        if (optimizeParameters) {
-            system->optimizeParameters          (system, alpha, beta);
+        system->setNumProcs                 (wrapper->m_numprocs);
+
+        if (wrapper->m_optimizeParameters) {
+            system->optimizeParameters          (system, wrapper->m_alpha, wrapper->m_beta);
         }
-        system->setSaveEnergies             (saveEnergies);
-        system->setSavePositions            (savePositions);
+        system->setSaveEnergies             (wrapper->m_saveEnergies);
+        system->setSavePositions            (wrapper->m_savePositions);
 
-        system->runMetropolisSteps          (numMyCycles, importanceSampling, showProgress, printToTerminal);
+        system->runMetropolisSteps          (wrapper->m_numMyCycles, wrapper->m_importanceSampling, wrapper->m_showProgress, wrapper->m_printToTerminal);
 
-        system->MPI_CleanUp                 (totalE, totalKE, totalPE, totalVariance, totalAcceptanceRate, finalMeanDistance,
-                                             timeStart, timeEnd, totalTime, numprocs, numberOfSteps);
-
-        system->mergeOutputFiles            (numprocs);
-
-        SquareWell* sqwell = new SquareWell(system, 1., 3., omega, false, false);
-        //CHECK(sqwell->computeLocalEnergy(system->getParticles()), 5);
+        system->MPI_CleanUp                 (wrapper->m_totalE, wrapper->m_totalKE, wrapper->m_totalPE, wrapper->m_totalVariance, wrapper->m_totalAcceptanceRate, wrapper->m_finalMeanDistance,
+                                             wrapper->m_timeStart, wrapper->m_timeEnd, wrapper->m_totalTime, wrapper->m_numprocs, wrapper->m_numberOfSteps);
+        // Merge the files from the nodes into one data file
+        system->mergeOutputFiles            (wrapper->m_numprocs);
     }
 }
 
 
 
-int main() {
+int main(int nargs, char* args[]) {
     return RunAllTests();
 }
 
--- vmc-solver/tests/wrapper.h
+++ vmc-solver/tests/wrapper.h
@@ -1,30 +1,45 @@
-#ifndef WRAPPER_H
-#define WRAPPER_H
+#ifndef WRAPPER_H 
+#define WRAPPER_H 
+#include <armadillo> 
+using namespace arma; 
 
-#include <armadillo>
-using namespace arma;
-
-class Wrapper
-{
+class Wrapper {
 public:
     Wrapper();
-
-    int m_N;
-    double m_posMin;
-    double m_posMax;
-    double m_omega_r;
-    int m_nMax;
-    int m_nPrimeMax;
-    int m_numberOfDimensions;
-    double m_h;
-
-    bool m_harmOscPotential;
 
     vec m_L;
     void setL(vec);
 
-    int m_numberOfEigstates;
+    int m_numprocs, m_my_rank;
+    double m_totalE, m_totalKE, m_totalPE, m_totalVariance, m_totalAcceptanceRate, m_finalMeanDistance;
+    double m_timeStart, m_timeEnd, m_totalTime;
+    int m_numMyCycles = m_numberOfSteps/m_numprocs;
 
+    int m_numberOfDimensions;
+    int m_numberOfParticles;
+    int m_numberOfSteps;
+    double m_omega;
+    double m_alpha;
+    double m_beta;
+    double m_gamma;
+    double m_a;
+    double m_stepLength;
+    double m_equilibration;
+    double m_dt;
+    double m_aElectrons;
+    double m_C;
+    bool m_analyticalKinetic;
+    bool m_importanceSampling;
+    bool m_repulsion;
+    bool m_quantumDots;
+    bool m_twobodyQD;
+    bool m_Jastrow;
+    bool m_optimizeParameters;
+    bool m_saveEnergies;
+    bool m_savePositions;
+    bool m_showProgress;
+    bool m_printToTerminal;
+    bool m_useCoeff; 
 
 };
 
