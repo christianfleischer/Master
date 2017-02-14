feb67f2ce765cc5c379fbb0c3ec232b4c5910376
Alexander Fleischer <fleisch91@gmail.com>
2017-02-14 12:35:56 +0100

    i dont even know

--- vmc-solver/tests/main.cpp
+++ vmc-solver/tests/main.cpp
@@ -39,14 +39,12 @@ SUITE(VMCsolver) {
     TEST(Initialization) {
 
         // Initialize MPI parallelization
-        //MPI_Init(&nargs, &args);
-        //MPI_Comm_size(MPI_COMM_WORLD, &wrapper->m_numprocs);
-        //MPI_Comm_rank(MPI_COMM_WORLD, &wrapper->m_my_rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &wrapper->m_numprocs);
+        MPI_Comm_rank(MPI_COMM_WORLD, &wrapper->m_my_rank);
         wrapper->m_numprocs = 1;
         wrapper->m_my_rank = 0;
-        //wrapper->m_timeStart = MPI_Wtime();
-        wrapper->m_numberOfDimensions  = 1;
-
+        wrapper->m_timeStart = MPI_Wtime();
+        wrapper->m_numberOfDimensions  = 2;
         vec L(wrapper->m_numberOfDimensions);
         L.fill(0.);
         L(0) = 0.;
@@ -54,28 +52,28 @@ SUITE(VMCsolver) {
 
         wrapper->m_numberOfParticles   = 2;
         wrapper->m_numberOfSteps       = (int) 1e6;
-        wrapper->m_omega            = 1.;
-        wrapper->m_alpha            = 0.98456;
-        wrapper->m_beta             = 0.40691;
-        wrapper->m_gamma            = 2.82843;
-        wrapper->m_a                = 0.0043;
-        wrapper->m_stepLength       = 0.5;
-        wrapper->m_equilibration    = 0.1;
-        wrapper->m_dt               = 0.01;
-        wrapper->m_aElectrons       = 1.;
-        wrapper->m_C                = 1.;
-        wrapper->m_analyticalKinetic  = false;
-        wrapper->m_importanceSampling = true;
-        wrapper->m_repulsion          = true;
-        wrapper->m_quantumDots        = true;
-        wrapper->m_twobodyQD          = false;
-        wrapper->m_Jastrow            = true;
-        wrapper->m_optimizeParameters = false;
-        wrapper->m_saveEnergies       = false;
-        wrapper->m_savePositions      = false;
-        wrapper->m_showProgress       = true;
-        wrapper->m_printToTerminal    = true;
-        wrapper->m_useCoeff 		  = false;
+        wrapper->m_omega               = 1.;
+        wrapper->m_alpha               = 0.98456;
+        wrapper->m_beta                = 0.40691;
+        wrapper->m_gamma               = 2.82843;
+        wrapper->m_a                   = 0.0043;
+        wrapper->m_stepLength          = 0.5;
+        wrapper->m_equilibration       = 0.1;
+        wrapper->m_dt                  = 0.01;
+        wrapper->m_aElectrons          = 1.;
+        wrapper->m_C                   = 1.;
+        wrapper->m_analyticalKinetic   = false;
+        wrapper->m_importanceSampling  = true;
+        wrapper->m_repulsion           = true;
+        wrapper->m_quantumDots         = true;
+        wrapper->m_twobodyQD           = false;
+        wrapper->m_Jastrow             = true;
+        wrapper->m_optimizeParameters  = false;
+        wrapper->m_saveEnergies        = false;
+        wrapper->m_savePositions       = false;
+        wrapper->m_showProgress        = true;
+        wrapper->m_printToTerminal     = true;
+        wrapper->m_useCoeff 		   = false;
 
         System* system = new System();
 
@@ -129,6 +127,7 @@ SUITE(VMCsolver) {
 
 
 int main(int nargs, char* args[]) {
+    MPI_Init(&nargs, &args);
     return RunAllTests();
 }
 
